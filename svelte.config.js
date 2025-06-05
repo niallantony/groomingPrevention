46d7ceaf5f64d3e61af0ce7fafbb003019832c70
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-06-05 20:11:42 +0900

    Tweak build

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,12 @@
 import adapter from "@sveltejs/adapter-static";
-const dev = process.argv.includes("dev");
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   kit: {
     adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: "build",
-      assets: "build",
-      fallback: undefined,
-      precompress: false,
-      strict: true,
+      fallback: "404.html",
     }),
-    paths: {
-      base: dev ? "" : "/groomingPrevention",
-    },
   },
 };
+
+export default config;
